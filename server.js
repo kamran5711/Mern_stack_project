3a7a5e8c30b64a5651c2fe426b4b5a1b7f5eec23
kamran5711 <muhammad.kamran.5711@gmail.com>
2020-07-01 00:09:20 -0700

    'changes'

--- server.js
+++ server.js
@@ -15,14 +15,13 @@ app.use('/api/auth', require('./routes/api/auth') );
 app.use('/api/profile', require('./routes/api/profile') );
 app.use('/api/posts', require('./routes/api/posts') );
 
-
 // serve static assets in production
 if(process.env.NODE_ENV === 'production'){
     // set static folder
-    app.use(express.static(path.join(__dirname, './client/build')))
+    app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', function(_, res) {
-      res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+      res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
         if (err) {
           res.status(500).send(err)
         }
